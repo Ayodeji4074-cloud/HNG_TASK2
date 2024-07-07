be3f28c860bb7d1c5165858e9b43054bd89967b9
Ayodeji4074-cloud <adeniyiemmanuel998@gmail.com>
2024-07-07 20:52:32 +0100

    hng

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ require('dotenv').config();
 const express = require('express');
 const { connectDB } = require('./config/db');
 
-
-
 const app = express();
 
 // Middleware
@@ -15,6 +13,12 @@ connectDB();
 // Routes
 app.use('/auth', require('./routes/authRoutes'));
 app.use('/api/organisations', require('./routes/organisationRoutes'));
+
+// Basic error handling
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
 
 const PORT = process.env.PORT || 7000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
-
-const app = require('./app');
+const app = require('./app'); // Make sure app.js is properly set up and exported
 
 const PORT = process.env.PORT || 8000;
 
@@ -8,5 +7,8 @@ app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
-
- 
+// Basic error handling
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
